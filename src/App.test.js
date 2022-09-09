0afed4f36c83464da8db12ffb858b0d47247ba2b
Jonathan Welton <jwelton@audionetwork.com>
2022-09-09 09:33:05 +0100

    Corrected the unit test title

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the page title', () => {
   render(<App />);
   const headerElement = screen.getByText(/Latest Released Albums/i);
   expect(headerElement).toBeInTheDocument();
