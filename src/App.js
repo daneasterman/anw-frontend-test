46c7920493e50f66690d55411e0baa65a16c07a2
Daniel Easterman <daniel.easterman@gmail.com>
2022-11-10 18:51:35 +0000

    WIP adding favourite track list

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import { getAlbums, getAlbumTracks } from './services/musicApiClient'
 
 function App() {
   const [albums, setAlbums] = useState([])
+  // const [favTrack, setFavTrack] = useState('')
+  const [favTracklist, setFavTrackList] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
       const albums = await (await getAlbums()).slice(0, 9)
-      console.log('albums', albums)
+      console.log(albums)    
 
       for await (const album of albums) {
         album.artworkUrl = await getAlbumArtworkUrl(album.number)
@@ -24,8 +26,32 @@ function App() {
     fetchData();
   }, []);
 
+  const addFavTrack = event => {
+    const value = event.target.value
+    // console.log(value)
+    const newList = favTracklist.concat({value});
+    console.log(newList)
+    setFavTrackList(newList)
+  };
+  
+  // const handleAdd = event => {
+  //   setFavTrack(event.target.value);
+  // };  
+
   return (
     <main>
+    <div className="flexColumn"> 
+      <h1 className="heading">My Favourite Tracks</h1>
+      <ul>      
+      {favTracklist?.map((key, item) => (        
+        <li key={key}>
+        {console.log('markup', item.value)}
+          <p>{item.value}</p>
+        </li>
+      ))}
+      </ul>
+    </div>
+
       <h1 className="heading">Latest Released Albums</h1>
         <ul className="grid">
           {albums?.map((album) => (
@@ -39,7 +65,13 @@ function App() {
                   />
                   <h3 style={{fontWeight: 'bold'}}>Album Tracks:</h3>
                   {album.tracks?.map((track) => (
+                  <div className="flexRow" key={track.id}>               
                       <p>{track.title}</p>
+                      <input                        
+                        value={track.title}                
+                        onChange={addFavTrack}                        
+                        type="radio" />
+                    </div>
                     )
                   )}
                 </li>                        
